52a5cc8bdef566cedc75cddc3c8ade13a9990d30
unknown <aspiremack1969@hotmail.com>
2019-09-11 15:06:09 +0700

    Initial project

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function handleEvent(event) {
 function handleMessageEvent(event) {
     var msg = {
         type: 'text',
-        text: 'สถาบันวิจัยและพัฒนาแห่ง มหาวิทยาลัยเทคโนโลยีราชมงคลรัตนโกสินทร์ เลขที่ 96 หมู่ 3 ถนนพุทธมณฑลสาย 5 ต.ศาลายา อ.พุทธมณฑล จ.นครปฐม 73170'
+        text: 'Hello'
     };
 
     var eventText = event.message.text.toLowerCase();
