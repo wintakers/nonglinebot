07161bae09efd84c2a99b9961e1e0a2cf09b0dbe
unknown <aspiremack1969@hotmail.com>
2019-09-13 13:53:28 +0700

    update

--- index.js
+++ index.js
@@ -90,6 +90,12 @@ function handleMessageEvent(event) {
             text: 'กำหนดการประชุม\n-	ติดต่อคุณพัชรี\nเอกสารที่ใช้ยืนต่อกองทุน\nhttp://www.ird.rmutr.ac.th/%e0%b8%81%e0%b8%ad%e0%b8%87%e0%b8%97%e0%b8%b8%e0%b8%99%e0%b8%aa%e0%b8%99%e0%b8%b1%e0%b8%9a%e0%b8%aa%e0%b8%99%e0%b8%b8%e0%b8%99%e0%b8%87%e0%b8%b2%e0%b8%99%e0%b8%a7%e0%b8%b4%e0%b8%88%e0%b8%b1%e0%b8%a2/ '
 
         }
+    }else if (eventText === 'คุยกับบอท') {
+        msg = {
+            'type': 'text',
+            text: 'สวัสดีค่ะท่านสามารถสอบถามเกี่ยวกับ ทุนวิจัย เบิกเงินวิจัยและกองทุนสนับสนุนงานวิจัย'
+
+        }
     }else if (eventText === 'location') {
         msg = {
             "type": "location",
