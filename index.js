40cada0ca9062b84f0aedd11fb70ca3d0e787801
unknown <aspiremack1969@hotmail.com>
2019-09-24 12:01:01 +0700

    update

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function handleMessageEvent(event) {
     }else if (eventText === 'กองทุนสนับสนุนงานวิจัย') {
         msg = {
             'type': 'text',
-            text: 'กำหนดการประชุม\n-	ติดต่อคุณพัชรี\nเอกสารที่ใช้ยืนต่อกองทุน\nhttp://www.ird.rmutr.ac.th/%e0%b8%81%e0%b8%ad%e0%b8%87%e0%b8%97%e0%b8%b8%e0%b8%99%e0%b8%aa%e0%b8%99%e0%b8%b1%e0%b8%9a%e0%b8%aa%e0%b8%99%e0%b8%b8%e0%b8%99%e0%b8%87%e0%b8%b2%e0%b8%99%e0%b8%a7%e0%b8%b4%e0%b8%88%e0%b8%b1%e0%b8%a2/ '
+            text: 'กำหนดการประชุม\n-ติดต่อคุณพัชรี\nเอกสารที่ใช้ยืนต่อกองทุน\nhttp://www.ird.rmutr.ac.th/%e0%b8%81%e0%b8%ad%e0%b8%87%e0%b8%97%e0%b8%b8%e0%b8%99%e0%b8%aa%e0%b8%99%e0%b8%b1%e0%b8%9a%e0%b8%aa%e0%b8%99%e0%b8%b8%e0%b8%99%e0%b8%87%e0%b8%b2%e0%b8%99%e0%b8%a7%e0%b8%b4%e0%b8%88%e0%b8%b1%e0%b8%a2/ '
 
         }
     }else if (eventText === 'คุยกับบอท') {
