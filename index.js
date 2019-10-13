2068407383bc32f4c16f1c0c58c5c72a0bba1e91
wintakers <56497164+wintakers@users.noreply.github.com>
2019-10-13 14:44:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,59 +59,8 @@ function handleMessageEvent(event) {
 
     let eventText = event.message.text.toLowerCase();
 
-    if (eventText === 'ขอที่อยู่') {
-        msg = {
-            'type': 'text',
-            text: address.MSG
-        }
-    } else if (eventText === 'สอบถาม') {
-        // console.dir();
-        msg = {
-            'type': 'text',
-            text: 'test bot'
-        }
-    }  else if (eventText === 'สอบถามหน่อยครับ') {
-        msg = {
-            'type': 'text',
-            text: query.MSG
-
-        }
-    } else if (eventText === 'สอบถามหน่อยค่ะ') {
-        msg = {
-            'type': 'text',
-            text: query.MSG
-
-        }
-    } else if (eventText === 'ถามไรหน่อย') {
-        msg = {
-            'type': 'text',
-            text: query.MSG
-
-        }
-    } else if (eventText === 'สวัสดีครับ') {
-        msg = {
-            'type': 'text',
-            text: query.MSG
-        }
-    } else if (eventText === 'สวัสดีค่ะ') {
-        msg = {
-            'type': 'text',
-            text: query.MSG
-
-        }
-    } else if (eventText === 'สวัสดี') {
-        msg = {
-            'type': 'text',
-            text: query.MSG
-        }
-    } else if (eventText === 'หวัดดี') {
-        msg = {
-            'type': 'text',
-            text: query.MSG
-
-        }
-    }
-    else if (eventText === 'report') {
+    
+    if (eventText === 'report') {
 
 
         db.all("SELECT * FROM question", [], (err, row) => {
@@ -144,84 +93,6 @@ function handleMessageEvent(event) {
             'text':data.id
         }
         
-    } else if (eventText === 'ทุนวิจัย') {
-        msg = {
-            'type': 'text',
-            text: capital.MSG
-
-        }
-    } else if (eventText === 'ขอรายละเอียดทุนวิจัย') {
-        msg = {
-            'type': 'text',
-            text: capital.MSG1
-
-        }
-    } else if (eventText === 'ทุนวิจัย2564') {
-        msg = {
-            'type': 'text',
-            text: capital.MSG2
-
-        }
-    } else if (eventText === 'ทุนวิจัย2563') {
-        msg = {
-            'type': 'text',
-            text: capital.MSG3
-
-        }
-    } else if (eventText === 'เบิกเงินวิจัย') {
-        msg = {
-            'type': 'text',
-            text: withdraw.MSG
-        }
-    } else if (eventText === 'ขอรายละเอียดเบิกเงินวิจัย') {
-        msg = {
-            'type': 'text',
-            text: withdraw.MSG1
-
-        }
-    } else if (eventText === 'เบิกเงินงวดที่1') {
-        msg = {
-            'type': 'text',
-            text: withdraw.MSG2
-
-        }
-    } else if (eventText === 'เบิกเงินงวดที่2') {
-        msg = {
-            'type': 'text',
-            text: withdraw.MSG3
-
-        }
-    } else if (eventText === 'เบิกเงินงวดที่3') {
-        msg = {
-            'type': 'text',
-            text: withdraw.MSG4
-
-        }
-    } else if (eventText === 'กองทุนสนับสนุนงานวิจัย') {
-        msg = {
-            'type': 'text',
-            text: fund.MSG
-
-        }
-    } else if (eventText === 'คุยกับบอท') {
-        msg = {
-            'type': 'text',
-            text: 'สวัสดีค่ะท่านสามารถสอบถามเกี่ยวกับ\n-ทุนวิจัย\n-เบิกเงินวิจัย\n-กองทุนสนับสนุนงานวิจัย\n-เอกสารดาวน์โหลด\n' +
-                'ท่านสามารถดูรายละเอียดโดยการพิมพ์ขอรายละเอียดแต่ละหัวข้อกับน้องบอทได้ เช่น ทุนวิจัย เป็นต้น'
-
-        }
-    } else if (eventText === 'สามารถติดต่อได้ทางไหนบ้าง') {
-        msg = {
-            'type': 'text',
-            text: 'สวัสดีค่ะท่านสามารถติดต่อ สถาบันวิจัยและพัฒนา มทร.รัตนโกสินทร์ ได้ตามช่องทางการติดต่อด้านล่างนี้\nFacebook : https://www.facebook.com/irdrmutr\nWebsite : https://ird.rmutr.ac.th\nEmail : ird.r@rmutr.ac.th , irdrmutr@hotmail.co.th\nสามารถติดต่อได้ที่ 02-441-6060 ต่อ 2420-2426'
-
-        }
-    } else if (eventText === 'เอกสารดาวน์โหลด') {
-        msg = {
-            'type': 'text',
-            text: 'สวัสดีค่ะท่านสามารถดาวน์โหลดเอกสารต่างๆได้ในลิงค์ด้านล่างนี้\nhttps://ird.rmutr.ac.th/formdownload/ '
-
-        }
     }  
     else {
         
