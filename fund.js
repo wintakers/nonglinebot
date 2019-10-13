df138eeec3e96dc7ecff1858122865327e3f3320
wintakers <56497164+wintakers@users.noreply.github.com>
2019-10-13 14:59:51 +0700

    Delete fund.js

--- fund.js
+++
@@ -1,8 +0,0 @@
-const fund ={
-    MSG : 'กำหนดการประชุม\n' +
-    '-ติดต่อคุณพัชรี\nเอกสารที่ใช้ยืนต่อกองทุน\n' +
-    'http://www.ird.rmutr.ac.th/%e0%b8%81%e0%b8%ad%e0%b8%87%e0%b8%97%e0%b8%b8%e0%b8%99%e0%b8%aa%e0%b8%99%e0%b8%b1%e0%b8%9a%e0%b8%aa%e0%b8%99%e0%b8%b8%e0%b8%99%e0%b8%87%e0%b8%b2%e0%b8%99%e0%b8%a7%e0%b8%b4%e0%b8%88%e0%b8%b1%e0%b8%a2/ '
-    
-}
-
-module.exports=fund
